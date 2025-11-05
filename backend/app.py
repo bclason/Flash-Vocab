67e8313e26197b2fbb95c85c232e754a6d119e9d
bclason <bclason@umich.edu>
2025-11-05 15:41:49 -0500

    flask debug

--- backend/app.py
+++ backend/app.py
@@ -406,5 +406,6 @@ app.config['PROPAGATE_EXCEPTIONS'] = True
 if __name__ == '__main__':
     import os
     port = int(os.environ.get('PORT', 5000))
-    debug_mode = os.environ.get('FLASK_ENV') == 'development'
-    app.run(host='0.0.0.0', port=port, debug=debug_mode)
+    # debug_mode = os.environ.get('FLASK_ENV') == 'development'
+    # app.run(host='0.0.0.0', port=port, debug=debug_mode)
+    app.run(host='0.0.0.0', port=port, debug=True)
